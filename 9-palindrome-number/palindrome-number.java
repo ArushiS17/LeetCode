bd3008ccb68c25738192ce7e452ddba9a3e428bc
Arushi Saxena <97784490+ArushiS17@users.noreply.github.com>
2024-06-03 17:35:54 +0530

    Time: 4 ms (100.00%) | Memory: 43.9 MB (54.52%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int rev=0;
+        int y=x;
+        if(x<0){
+            return false;
+        }
+        while(x>0){
+            int r= x%10;
+            rev=rev*10+r;
+            x=x/10;
+        }
+        if(rev==y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
