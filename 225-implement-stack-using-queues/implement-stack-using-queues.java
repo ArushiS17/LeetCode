e2cb56a1ee4437ab827e81bbf2522c5bab173d49
Arushi Saxena <97784490+ArushiS17@users.noreply.github.com>
2024-07-10 19:39:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (23.50%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -7,8 +7,8 @@ class MyStack {
     public void push(int x) {
         q.add(x);
         for(int i = 1; i<q.size(); i++){
-            q.add(q.peek());
-            q.remove();
+            q.add(q.remove());
+            
         }
     }
     
