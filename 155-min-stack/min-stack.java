4480c5c21ab8557e9279164bc5efdbc42dd4f140
Arushi Saxena <97784490+ArushiS17@users.noreply.github.com>
2024-08-09 17:47:25 +0530

    Time: 5 ms (36.64%) | Memory: 44.8 MB (51.80%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,51 +1,41 @@
-import java.util.Stack;
-
 class MinStack {
-    Stack<Long> st; // Change to Long to handle overflow
-    long mini; // Change to long to handle larger range of values
+
+    private Stack<Integer>stack;
+    private Stack<Integer>minStack;
 
     public MinStack() {
-        st = new Stack<>();
-        mini = Long.MAX_VALUE; // Initialize with Long.MAX_VALUE
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
-
+    
     public void push(int val) {
-        if (st.isEmpty()) {
-            mini = val;
-            st.push(0L); // Push 0L when the first element is added
-        } else {
-            st.push(val - mini); // Store the difference instead of modified value
-            if (val < mini) {
-                mini = val; // Update mini when a new minimum is encountered
-            }
+        stack.push(val);
+        if(minStack.isEmpty() || val<=minStack.peek())
+        {
+            minStack.push(val);
         }
     }
-
+    
     public void pop() {
-        if (st.isEmpty()) return;
+         if(stack.peek().equals(minStack.peek()))
+         {
+            minStack.pop();
+         }
+     stack.pop();
 
-        long diff = st.pop();
-        if (diff < 0) {
-            mini = mini - diff; // Restore the previous minimum
-        }
     }
-
+    
     public int top() {
-        if (st.isEmpty()) return -1;
+          
+        return  stack.peek();
 
-        long diff = st.peek();
-        if (diff > 0) {
-            return (int) (mini + diff);
-        } else {
-            return (int) mini; // Return mini if top element is modified
-        }
+
     }
-
+    
     public int getMin() {
-        return (int) mini;
+        return minStack.peek();
     }
 }
-
 
 /**
  * Your MinStack object will be instantiated and called as such:
